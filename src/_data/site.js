72b62a57c82cc12667708d712ef49de337e38289
t1dem <neptunescodm@gmail.com>
2026-03-24 18:37:56 +0100

    Add temporary maintenance mode

---
+++ src/_data/site.js
@@ -0,0 +1,9 @@
+module.exports = {
+  maintenanceMode: true,
+  maintenance: {
+    title: "Site en maintenance",
+    message: "Notre site est actuellement en maintenance.",
+    details: "Nous mettons tout en oeuvre pour le remettre en ligne dans les plus brefs delais.",
+    brandName: "PEN'HOUSE Conciergerie"
+  }
+};
